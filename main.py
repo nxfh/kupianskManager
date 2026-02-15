e2529f5b3d492a3af364f185319fa3f6c1fcfeb7
= <=>
2026-02-15 15:54:01 +0300

    Added separated methods for waiting before next poll creation and creating poll.

--- main.py
+++ main.py
@@ -9,27 +9,17 @@ from discord.ext import tasks
 logger = logging.getLogger("Main")
 streamHandler = logging.StreamHandler()
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+client = discord.Client(intents = intents)
+secondsBeforeNextPoll = 0
 
 streamHandler.setLevel(logging.INFO)
 streamHandler.setFormatter(logging.Formatter("[%(asctime)s - %(name)s]: %(message)s"))
 logger.setLevel(logging.INFO)
 logger.addHandler(streamHandler)
 
-@tasks.loop(seconds=604800)
-async def startPoll():
-    await client.wait_until_ready()
-
-    channel = client.get_channel(1469769978285916504)
-    poll = discord.Poll(question="Время:?", duration=timedelta(hours=1.0))
-
-    poll.add_answer(text="11pm")
-    poll.add_answer(text="11.30pm")
-    poll.add_answer(text="12pm+")
-    poll.add_answer(text="Не приду")
-
-    await channel.send(f"<@&1155111077189799986>\n`ГОЛОСОВАНИЕ.mp3`", file=discord.File("img/gif.gif"))
-    await channel.send(poll=poll)
+@tasks.loop(seconds = 604800)
+async def createPollTimer():
+    createPoll()
 
 @client.event
 async def on_ready():
@@ -37,25 +27,46 @@ async def on_ready():
     logger.info(client.user.name)
     logger.info(client.user.id)
     logger.info("-----")
-    
-    secondsSinceLastInterrogation = (int(time.time()) - 241200) % 604800
-    
-    if secondsSinceLastInterrogation > 0:
-        secondsBeforeNextInterrogation = 604800 - secondsSinceLastInterrogation
 
-        while secondsBeforeNextInterrogation > 0:
-            logger.info(f"Awaiting {secondsBeforeNextInterrogation} seconds...")
+    secondsSinceLastPoll = (int(time.time()) - 212400 ) % 604800
 
-            if secondsBeforeNextInterrogation > 10:
-                await asyncio.sleep(10)
+    if secondsSinceLastPoll > 0:
+        secondsBeforeNextPoll = 604800 - secondsSinceLastPoll
 
-                secondsBeforeNextInterrogation = secondsBeforeNextInterrogation - 10
-            else:
-                await asyncio.sleep(secondsBeforeNextInterrogation)
+        if secondsSinceLastPoll <= 32400:
+            createPoll()
 
-                secondsBeforeNextInterrogation = 0
+    await wait(secondsBeforeNextPoll)
+    logger.info("Starting poll timer...")
+    createPollTimer.start()
 
-    logger.info("Starting poll...")
-    startPoll.start()
+async def createPoll():
+    await client.wait_until_ready()
+    logger.info("Creating poll...")
+
+    channel = client.get_channel(1469769978285916504)
+    poll = discord.Poll(question = "Время:?", duration = timedelta(hours = 1.0))
+
+    poll.add_answer(text="11pm")
+    poll.add_answer(text="11.30pm")
+    poll.add_answer(text="12pm+")
+    poll.add_answer(text="Не приду")
+
+    await channel.send(f"<@&1155111077189799986>\n`ГОЛОСОВАНИЕ.mp3`", file = discord.File("img/gif.gif"))
+    await channel.send(poll = poll)
+    logger.info("Poll created")
+
+async def wait(seconds):
+    while seconds > 0:
+        logger.info(f"Awaiting {seconds} seconds...")
+
+        if seconds > 10:
+            await asyncio.sleep(10)
+
+            seconds -= 10
+        else:
+            await asyncio.sleep(seconds)
+
+            seconds = 0
 
 client.run(os.getenv("API_KEY", ""))
